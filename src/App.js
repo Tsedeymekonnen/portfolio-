46617f1f666623996d6aadad75fc2e5003b0cda1
Tsedeymekonnen <tsedey1993@gmail.com>
2023-11-09 10:27:57 -0700

    try

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <BrowserRouter>
       <Header />
       <Routes>
+        <Route  path="/portfolio-" element={<Home/>} />
         <Route  path="/" element={<Home/>} />
         <Route  path="/AboutUs.js" element={<AboutUs/>} />
         <Route  path = "/ContactUs.js" element = {<ContactUs/>}/> 
